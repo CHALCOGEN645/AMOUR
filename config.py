9fdb2d36b53d45203414a3f83b1d4217fbb26870
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-06-27 14:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ class Config(object):
     ARQ_API_KEY = "TBPYLF-SIOYFX-JALTSV-QEAMXE-ARQ"
     SPAMWATCH_API = "t9HHtrsmy7faPQWloX8xCvdZK~puDP2RnHLpb~qijQqDj94mhcMQdDP_xO0a_Iwe"
     TOKEN = getenv("TOKEN")
-    OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "JARVIS_V2")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Dora_Hub")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1002014167331"))
+    OWNER_ID = int(getenv("OWNER_ID", 6536914119)
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "chalcogenxd")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "penguin_x_support")
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002041048920"))
     MONGO_URI = getenv("MONGO_DB_URI")
-    DB_NAME = getenv("DB_NAME", "AvaRobot")
+    DB_NAME = getenv("DB_NAME", "Amour")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
     DATABASE_URL = getenv("DATABASE_URL", None)
 
